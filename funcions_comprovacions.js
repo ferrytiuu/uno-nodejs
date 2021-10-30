5fb5dc2dfa451eeecfa6ea968f137f55e5e124b8
fesolo <fesolo2122daw2@protonmail.com>
2021-10-30 13:38:27 +0200

    30/10/2021

---
+++ funcions_comprovacions.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    comprobar_partida: function (idPartida,partidas) {
+        let partida = partidas.find(a => a.id === idPartida);
+        return partida;
+    },
+    comprobar_jugador: function (idPartida,nombre,jugadors) {
+        let jugador = jugadors.find(a => a.id === idPartida && a.nombre === nombre);
+        return jugador;
+    }
+    
+  };
+  
+// var server = require("./ex06_mitjana.js");
--- uno_cartas_diversion.js
+++ uno_cartas_diversion.js
@@ -6,18 +6,30 @@
 
 
 const express = require('express');
+const { comprobar_jugador } = require('./funcions_comprovacions');
+var comp = require('./funcions_comprovacions');
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) // per analitzar les peticions HTTP que portin JSON al body
 
 //Cartes originals
+/*
 const cartas = [
     { num: 2, color: 'vermell' },
     { num: 8, color: 'blau' },
     { num: 7, color: 'groc' },
     { num: 4, color: 'verd' }
 ];
+*/
+let cartas = [];
+for (let index = 0; index <= 9; index++) {
+    cartas.push({num : index , color : 'vermell'})
+    cartas.push({num : index , color : 'blau'})
+    cartas.push({num : index , color : 'groc'})
+    cartas.push({num : index , color : 'verd'})
+}
 
 
 let jugadors = [];
@@ -25,83 +37,117 @@ let partidas = [];
 let pila = [];
 
 app.post('/iniciarJoc/codiPartida', (req, res) => { //POST per començar la partida amb un ID
-    let partida = { 'id': req.body.idPartida, 'cartas': cartas, "pila": pila };
+    let partida = { 'id': req.body.idPartida, 'cartas': cartas, "pila": pila, turno : 1 ,NumJugadores : 0 };
     partidas.push(partida);
     res.send(partida);
-    console.log(partida);
+    console.log(partidas);
 });
 
 app.post('/unirse', (req, res) => { // POST per unir-se a la partida amb el codi i un nom d'usuari. Si el nom no existeix, crea un usuari amb aquest
-    let partida = partidas.find(a => a.id === req.body.idPartida);
-    console.log(partida);
+    
+    
+    let partida = comp.comprobar_partida(req.body.idPartida,partidas);
     if (partida == undefined) {
         return res.send("Partida no existeix");
     }
-    let jugador = jugadors.find(a => a.id === req.body.idPartida && a.nombre === req.body.nombre);
+    let jugador = comp.comprobar_jugador(req.body.idPartida,req.body.nombre,jugadors);
     if (jugador == undefined) {
-        let jugador = { 'id': req.body.idPartida, 'nombre': req.body.nombre, 'cartasJugador': [] };
+        let jugador = { 'id': req.body.idPartida, 'nombre': req.body.nombre, 'numero' : partida.NumJugadores+1, 'cartasJugador': [] };
         jugadors.push(jugador);
+        
+        partida.NumJugadores+=1;
+        console.log("Info de partidas "+JSON.stringify(partidas));
         return res.send("Jugador " + req.body.nombre);
+        
     }
     else {
         return res.send("Jugador ja registrat");
     }
+    
 });
-app.get('/obtenirCarta/:Usuari/:codiPartida', (req, res) => { //GET per obtenir una carta aleatòria de l'array cartas, utilitzant Math.random
-    let jugador = verificarUsuario(req.params.codiPartida, req.params.Usuari)
+app.get('/obtenirCarta/:Usuari/:idPartida', (req, res) => { //GET per obtenir una carta aleatòria de l'array cartas, utilitzant Math.random
+    
+    
+    let partida = comp.comprobar_partida(req.params.idPartida,partidas);
+    if (partida == undefined) {
+        return res.send("Partida no existeix");
+    }
+    
+    
+    let jugador = comp.comprobar_jugador(req.params.idPartida, req.params.Usuari,jugadors);
     if ( jugador == undefined) {
         return res.send("Usuario o codi de partida incorrecte");
     }
-    let partida = partidas.find(a => a.id === req.params.codiPartida);
+
     let carta = partida.cartas[Math.floor(Math.random() * partida.cartas.length)];
-    
     let indice = partida.cartas.indexOf(carta);
-    partida.cartas.splice(indice, 1);
-    
-    //console.log(partida);
-
+    partida.cartas.splice(indice, 1);   
     jugador.cartasJugador.push(carta);
-
-    //console.log(jugador.cartasJugador);
     res.send(jugador.cartasJugador);
 
 });
 
 
-app.put('/tirarCarta/:Usuari/:codiPartida/:numero/:color', (req, res) => { //GET per obtenir una carta aleatòria de l'array cartas, utilitzant Math.random
-    let partida = partidas.find(a => a.id === req.body.codiPartida);
-    console.log(partida);
+app.put('/tirarCarta', (req, res) => { //GET per obtenir una carta aleatòria de l'array cartas, utilitzant Math.random
+    
+    
+    let partida = comp.comprobar_partida(req.body.idPartida,partidas);
+    //console.log(partida);
     if (partida == undefined) {
         return res.send("Partida no existeix");
     }
 
-    let jugador = verificarUsuario(req.body.codiPartida, req.body.Usuari)
+    let jugador = comp.comprobar_jugador(req.body.idPartida, req.body.Usuari,jugadors);
     if ( jugador == undefined) {
         return res.send("Usuario o codi de partida incorrecte");
+    }
+
+    if( jugador.numero !== partida.turno){
+        return res.send("No es el turno del usuario, tu turno es el: " +jugador.numero+" y el turno actual es el "+partida.turno);
     }
 
     //Verificar si color i numero de carta existeixen
 
     
-    let indice = jugador.cartasJugador.indexOf({num :req.body.num, color : req.body.color });
-    if(indice !=1){
-        console.log("Carta a llençar "+jugador.cartasJugador[indice])
-        jugador.cartasJugador[indice];
+    let indice = jugador.cartasJugador.indexOf({"num" : req.body.num, "color" : req.body.color });
+    let carta = jugador.cartasJugador.find( fruta => fruta.num == req.body.num && fruta.color == req.body.color  );
+    
+    
+    //console.log({num : req.body.num*1, color : req.body.color });
+    //console.log(indice);
+    if(carta != undefined ){
 
-        partida.pila.push(jugador.cartasJugador[indice]);
-        res.send("Ta bien");
+        let indice = jugador.cartasJugador.indexOf(carta);
+        console.log("Carta a llençar "+indice+" "+JSON.stringify(jugador.cartasJugador[indice]));
+        //jugador.cartasJugador[indice];
+
+        partida.pila.push(carta);
+        //res.send("Ta bien");
+        jugador.cartasJugador.splice(indice, 1);
+        console.log("Info de la pila "+JSON.stringify(partida.pila));
+
+
+        //Cambiar turno
+
+        if(partida.turno+1 > partida.NumJugadores){
+            partida.turno=1;
+        }
+        else{
+            partida.turno +=1;
+        }
+        console.log("Turno actual: "+partida.turno);
+        return res.send(jugador.cartasJugador);
+        
     }
     else{
         return res.send("No existeix la carta");
     }
-    partida.cartas.splice(indice, 1);
+    
     
     //console.log(partida);
 
-    jugador.cartasJugador.push(carta);
-
     //console.log(jugador.cartasJugador);
-    res.send(jugador.cartasJugador);
+    
 
 });
 
@@ -114,10 +160,7 @@ app.get('/comprobarPila/:codiPartida', (req, res) => { //
 
 });
 
-function verificarUsuario(idPartida, nombre) {
-    let jugador = jugadors.find(a => a.id === idPartida && a.nombre === nombre);
-    return jugador;
-}
+
 
 
 app.listen(3000, () => console.log('inici servidor'));
